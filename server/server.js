51e4b056adf43a966435eb05a253830d2aed051c
Silvana Donato <silvana.donato@accenture.com>
2018-07-04 11:36:55 +0200

    add get todos route

--- server/server.js
+++ server/server.js
@@ -23,6 +23,12 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then( (todos) => {
+        res.send({todos});
+    }, (e => res.status(400).send(err)))
+});
+
 
 app.listen(3000, () => {
     console.log('Listening to port 3000');
