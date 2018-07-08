74381bf6598deaf5d220eac54624b826963a0c37
Silvana Donato <silvana.donato@accenture.com>
2018-07-08 10:30:55 +0100

    delete user

--- server/models/user.js
+++ server/models/user.js
@@ -55,6 +55,16 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.methods.removeToken = function(token) {
+    var user = this;
+    return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    });
+};
+
+
 // this is a model (class) function
 UserSchema.statics.findByToken = function(token) {
     var User = this;
